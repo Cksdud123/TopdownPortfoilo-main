dcdeb8ec7ec88b0c859e6ad287326282f94357a3
Cksdud123 <128467585+Cksdud123@users.noreply.github.com>
2024-05-06 14:31:52 +0900

    Gravity 구현 완료

--- Assets/Scripts/Normal State/MovementStateManager.cs
+++ Assets/Scripts/Normal State/MovementStateManager.cs
@@ -25,6 +25,15 @@ public class MovementStateManager : MonoBehaviour
     public WalkState Walk = new WalkState();
     public RunState Run = new RunState();
 
+    [SerializeField] float groundYOffset; // 땅에서의 높이 오프셋
+    [SerializeField] LayerMask groundMask; // 땅을 확인하기 위한 레이어 마스크
+    Vector3 spherePos; // 구의 위치
+
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpForce = 10;
+    [HideInInspector] public bool jumped; // 점프 여부
+    Vector3 velocity; // 속도 벡터
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -36,6 +45,7 @@ public class MovementStateManager : MonoBehaviour
     private void Update()
     {
         Move();
+        Gravity();
 
         anim.SetFloat("hInput", hInput);
         anim.SetFloat("vInput", vInput);
@@ -54,5 +64,18 @@ public class MovementStateManager : MonoBehaviour
 
         dir = transform.forward * vInput + transform.right * hInput;
         characterController.Move(dir.normalized * currentMoveSpeed * Time.deltaTime);
+    }
+    public bool IsGrounded()
+    {
+        spherePos = new Vector3(transform.position.x, transform.position.y - groundYOffset, transform.position.z);
+        if (Physics.CheckSphere(spherePos, characterController.radius - 0.05f, groundMask)) return true;
+        return false;
+    }
+    void Gravity()
+    {
+        if (!IsGrounded()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0) velocity.y = -2;
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
